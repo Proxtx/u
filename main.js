8d19207fd504f15a750a1ff2c58ee87212ea9795
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-22 21:17:22 +0200

    remove unnecessary imports from main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 import { listen } from "@proxtx/framework";
 import config from "@proxtx/config";
-import { clients } from "./private/clients.js";
-import { apps } from "./private/apps.js";
-
-console.log(clients);
 
 await listen(config.port);
 console.log("Server running on port:", config.port);
