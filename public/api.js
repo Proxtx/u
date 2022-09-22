58e2ba8da69fc4640ea7d98651b211f13cd2afc4
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-22 21:37:26 +0200

    make apps and app definitions public

--- public/api.js
+++ public/api.js
@@ -1,8 +1,19 @@
 import { apps } from "../private/apps.js";
 import { auth } from "./meta.js";
+import config from "@proxtx/config";
 
 export const execute = async (pwd, appIndex, method, args) => {
   if (!auth(pwd)) return;
   let app = apps[appIndex];
   if (app.definitions?.methods[method]) return await app[method](...args);
+};
+
+export const getApps = (pwd) => {
+  if (!auth(pwd)) return;
+  return config.apps;
+};
+
+export const getDefinitions = (pwd, appIndex) => {
+  if (!auth(pwd)) return;
+  return apps[appIndex]?.definitions;
 };
